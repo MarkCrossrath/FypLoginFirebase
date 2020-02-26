605f4e985a5f213521ec04f8973728850f4a1a45
MarkCrossrath <cross672@gmail.com>
2020-02-26 17:45:28 +0000

    clean up nad picasso error fixed

--- app/src/main/java/com/example/login/MyAdapter.java
+++ app/src/main/java/com/example/login/MyAdapter.java
@@ -1,70 +1,87 @@
 package com.example.login;
 
 import android.content.Context;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
+import java.util.List;
 
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ImageViewHolder> {
+    private Context mContext;
+    private List<Event> mEvents;
 
-    Context context;
-    ArrayList<Event> event;
-
-    public MyAdapter(Context c , ArrayList<Event> e)
-    {
-        context = c;
-       event = e;
-    }
-
-    @NonNull
-    @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.row,parent,false));
-
+    public MyAdapter(Context context, List<Event> event) {
+        mContext = context;
+        mEvents = event;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.title.setText(event.get(position).getTitle());
-        holder.description.setText(event.get(position).getDescription());
-        holder.location.setText(event.get(position).getLocation());
-        holder.date.setText(event.get(position).getDate());
+    public ImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.row, parent, false);
+        return new ImageViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ImageViewHolder holder, final int position) {
+        Event uploadCurrent = mEvents.get(position);
+        holder.textViewTitle.setText(uploadCurrent.getTitle());
+        holder.textViewDescription.setText(uploadCurrent.getDescription());
+        holder.textViewDate.setText(uploadCurrent.getDate());
+        holder.textViewLocation.setText(uploadCurrent.getLocation());
+        Picasso.get().load(uploadCurrent.getImage()).into(holder.imageView);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
 
 
-        Picasso.get().load(event.get(position).getImage()).into(holder.image);
+            }
+        });
+
+
+
+
 
     }
 
     @Override
     public int getItemCount() {
-        return event.size();
+        return mEvents.size();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder
-    {
-        TextView title,description,location,date;
-        ImageView image;
+    public class ImageViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewTitle;
+        public ImageView imageView;
+        public TextView textViewDescription;
+        public TextView textViewLocation;
+        public TextView textViewDate;
 
-        public MyViewHolder(View itemView) {
+
+        public ImageViewHolder(View itemView) {
             super(itemView);
-            title =  itemView.findViewById(R.id.rTitleTv);
-            description =  itemView.findViewById(R.id.rDescriptionTv);
-            image =  itemView.findViewById(R.id.rImageView);
-            location = itemView.findViewById(R.id.rLocationTv);
-            date= itemView.findViewById(R.id.rDateTv);
+
+            textViewTitle = itemView.findViewById(R.id.rTitleTv);
+            imageView = itemView.findViewById(R.id.rImageView);
+            textViewDescription = itemView.findViewById(R.id.rDescriptionTv);
+            textViewDate = itemView.findViewById(R.id.rDateTv);
+            textViewLocation = itemView.findViewById(R.id.rLocationTv);
+
+
+
+
         }
 
     }
+
+
 }
