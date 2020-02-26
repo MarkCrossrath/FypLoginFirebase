455c77bb96541d4e963e35ad13cd3d6069faf73d
MarkCrossrath <cross672@gmail.com>
2020-02-26 17:40:43 +0000

    Created file

---
+++ app/src/main/java/com/example/login/TicketSelectionActivity.java
@@ -0,0 +1,28 @@
+package com.example.login;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TicketSelectionActivity extends AppCompatActivity {
+
+
+    Button button1;
+    Button button2;
+    Button button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ticket_selection);
+
+      //  button1 = findViewById(R.id.button_1);
+      //  button2 = findViewById(R.id.button_2);
+       // button3 = findViewById(R.id.button_3);
+
+
+
+
+    }
+}
