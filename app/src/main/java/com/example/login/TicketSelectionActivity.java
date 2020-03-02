db96b3b667cf309378066986205ab099a84363be
MarkCrossrath <cross672@gmail.com>
2020-03-02 17:28:44 +0000

    Created class buttons working

--- app/src/main/java/com/example/login/TicketSelectionActivity.java
+++ app/src/main/java/com/example/login/TicketSelectionActivity.java
@@ -1,6 +1,8 @@
 package com.example.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +19,17 @@ public class TicketSelectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ticket_selection);
 
-      //  button1 = findViewById(R.id.button_1);
-      //  button2 = findViewById(R.id.button_2);
-       // button3 = findViewById(R.id.button_3);
+        button1 = findViewById(R.id.button_1);
+        button2 = findViewById(R.id.button_2);
+        button3 = findViewById(R.id.button_3);
 
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TicketSelectionActivity.this,TicketOneActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
 
