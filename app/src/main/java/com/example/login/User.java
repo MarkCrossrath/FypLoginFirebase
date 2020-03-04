ed15fcce68fb419ab5bea798c032aac6d113efba
MarkCrossrath <cross672@gmail.com>
2020-03-04 09:29:47 +0000

    Added ticket getter and setter

--- app/src/main/java/com/example/login/User.java
+++ app/src/main/java/com/example/login/User.java
@@ -1,15 +1,24 @@
 package com.example.login;
 
 public class User {
-    public String email, preference;
+    public String email, preference, ticket;
 
     public User(){
 
     }
 
 
-    public User(String email, String preference) {
+    public User(String email, String preference, String ticket) {
         this.email = email;
         this.preference = preference;
+        this.ticket = ticket;
+    }
+
+    public String getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(String ticket) {
+        this.ticket = ticket;
     }
 }
