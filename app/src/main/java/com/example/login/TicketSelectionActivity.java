8a3c759081e40fe7c3af552591dc7367a7c0e378
MarkCrossrath <cross672@gmail.com>
2020-03-04 09:30:21 +0000

    added image view and text view

--- app/src/main/java/com/example/login/TicketSelectionActivity.java
+++ app/src/main/java/com/example/login/TicketSelectionActivity.java
@@ -4,24 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TicketSelectionActivity extends AppCompatActivity {
 
 
+    ImageView eventImage;
     Button button1;
     Button button2;
     Button button3;
+    TextView selectionTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ticket_selection);
 
-        button1 = findViewById(R.id.button_1);
-        button2 = findViewById(R.id.button_2);
-        button3 = findViewById(R.id.button_3);
+        Intent intent = getIntent();
+        intent.getStringExtra("key");
+
+        selectionTitle = findViewById(R.id.selection_title);
+        eventImage = findViewById(R.id.event_Image);
+        button1 = findViewById(R.id.ticket_one);
+        button2 = findViewById(R.id.ticket_two);
+        button3 = findViewById(R.id.ticket_three);
+
+
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
