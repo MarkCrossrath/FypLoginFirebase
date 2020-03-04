fe6866fb6bc91c2df11e5b020c3d900c7b72c78a
MarkCrossrath <cross672@gmail.com>
2020-03-04 09:32:05 +0000

    Added alert dialog

--- app/src/main/java/com/example/login/MyAdapter.java
+++ app/src/main/java/com/example/login/MyAdapter.java
@@ -1,8 +1,12 @@
 package com.example.login;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,20 +17,31 @@ import android.widget.Toast;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
+import java.security.Key;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.firebase.ui.auth.AuthUI.TAG;
 
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ImageViewHolder> {
     private Context mContext;
     private List<Event> mEvents;
+    private DatabaseReference mDatabaseRef;
+    private DataSnapshot dataSnapshot;
 
+    private static final String TAG = "MyAdapter";
 
-
-
-    public MyAdapter(Context context, List<Event> event ) {
+    public MyAdapter(Context context , List<Event> event) {
         mContext = context;
         mEvents = event;
 
@@ -34,14 +49,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ImageViewHolder> {
     }
 
     @Override
-    public ImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(mContext).inflate(R.layout.row, parent, false);
+    public ImageViewHolder onCreateViewHolder(ViewGroup parent , int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.row , parent , false);
         return new ImageViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(ImageViewHolder holder, final int position) {
-        Event uploadCurrent = mEvents.get(position);
+    public void onBindViewHolder(ImageViewHolder holder , final int position) {
+        final Event uploadCurrent = mEvents.get(position);
         holder.textViewTitle.setText(uploadCurrent.getTitle());
         holder.textViewDescription.setText(uploadCurrent.getDescription());
         holder.textViewDate.setText(uploadCurrent.getDate());
@@ -54,14 +69,30 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ImageViewHolder> {
             public void onClick(View view) {
 
 
-                Intent intent = new Intent(mContext,TicketSelectionActivity.class);
+                Intent intent = new Intent(mContext , TicketSelectionActivity.class);
+                intent.putExtra("image View",uploadCurrent.getTitle());
 
-               mContext.startActivity(intent);
+                mContext.startActivity(intent);
 
 
             }
         });
+
+        holder.parentlayout.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+
+
+                showAlertDialog(v);
+
+                return false;
+            }
+        });
+
+
     }
+
 
     @Override
     public int getItemCount() {
@@ -77,8 +108,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ImageViewHolder> {
         public CardView parentlayout;
 
 
-
-        public ImageViewHolder(View itemView ) {
+        public ImageViewHolder(View itemView) {
             super(itemView);
 
             textViewTitle = itemView.findViewById(R.id.rTitleTv);
@@ -89,13 +119,48 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ImageViewHolder> {
             parentlayout = itemView.findViewById(R.id.parent_layout);
 
 
-
-
-
         }
 
 
     }
+
+
+    public void showAlertDialog(View v) {
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+        alert.setTitle("Delete Event");
+        alert.setMessage("Are you sure you want to delete this event?");
+        alert.setPositiveButton("Yes" , new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog , int which) {
+
+               // DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+              //  String key = getIntent().getStringExtra("key");
+               // ref.child("EventData").child(key).removeValue();
+
+
+                Toast.makeText(mContext, "deleted", Toast.LENGTH_LONG).show();
+                Log.d(TAG,"Remove Query was called !!!!!!!!!!!!");
+
+
+
+            }
+        });
+        alert.setNegativeButton("No" , new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog , int which) {
+                Toast.makeText(mContext, "ok perfect", Toast.LENGTH_LONG).show();
+
+            }
+        });
+        alert.create().show();
+
+    }
+
+
+
+
+
 
 
 }
